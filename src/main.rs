43612d23721580446047eeadd057514969a3c3d6
Dmitry Shinkaruk <dimashink@gmail.com>
2021-03-27 15:08:02 +0300

    Sending pings over UDP with specified interval

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use std::time::{Duration, Instant};
 use std::str;
 use std::sync::Arc;
 use std::cmp::max;
-use clap::App;
+use std::collections::VecDeque;
 
+use clap::App;
 use mio::net::UdpSocket;
 use mio::{Events, Interest, Poll, Token, Waker};
 
@@ -111,23 +112,43 @@ fn run_udp_server(local_address: &str, local_port: &str) -> Result<()> {
         poll.poll(&mut events, None)?;
         for event in events.iter() {
             if event.is_readable() {
-                let mut buf = [0; 1024];
-                let (amt, src) = socket.recv_from(&mut buf).unwrap();
-                socket.send_to(&buf[..amt], src).unwrap();
+                loop {
+                    let mut buf = [0; 8];
+                    match socket.recv_from(&mut buf) {
+                        Ok((amt, src)) => {
+                            socket.send_to(&buf[..amt], src).unwrap();
+                        }
+                        Err(_) => {
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
 }
 
-// TODO: Set ttl on the messages
+// TODO: Set tos on the messages
 // TODO: Set length of the messages
 
-fn send_buf_to_udp_sock(socket: &UdpSocket ,buf: &[u8], target: SocketAddr) -> Result<Instant> {
+fn send_buf_to_udp_sock(socket: &UdpSocket,
+                        target: SocketAddr,
+                        current_counter: &mut u64,
+                        msg_queue: &mut VecDeque<TimeStamp>) -> Result<Instant> {
     let now = Instant::now();
-    socket.send_to(buf, target)?;
+    let buf: [u8; 8] = current_counter.to_be_bytes();
+    socket.send_to(&buf, target)?;
+    msg_queue.push_back(TimeStamp{id: *current_counter, timestamp: now});
+    *current_counter += 1;
     return Ok(now);
 }
 
+struct TimeStamp {
+    id: u64,
+    timestamp: Instant,
+}
+
+// TODO: Refactor this function. It is too big now.
 fn run_udp_client(address: &str, port: &str, interval: Option<u64>) -> Result<()> {
     // Times in millis
     const DEFAULT_READ_RESPONSE_TIMEOUT: u64 = 1000;
@@ -136,12 +157,15 @@ fn run_udp_client(address: &str, port: &str, interval: Option<u64>) -> Result<()
         None => DEFAULT_READ_RESPONSE_TIMEOUT
     };
 
+    let mut ts_queue: VecDeque<TimeStamp> = VecDeque::new();
+    let mut msg_id_counter: u64 = 0;
+
     println!("Running UDP client sending pings to {}:{}", address, port);
 
     let mut socket = UdpSocket::bind("0.0.0.0:0".parse().unwrap())?;
 
     let mut poll = Poll::new()?;
-    let mut events = Events::with_capacity(128);
+    let mut events = Events::with_capacity(1024);
     poll.registry()
         .register(&mut socket,
                   UDP_SOCKET,
@@ -157,17 +181,24 @@ fn run_udp_client(address: &str, port: &str, interval: Option<u64>) -> Result<()
         });
     }
 
-    // TODO: Set message ID into each echo request
-    let snd_buf = b"Ping message";
     let target_addr: SocketAddr = format!("{}:{}", address, port).parse().unwrap();
-    let mut now = send_buf_to_udp_sock(&socket, snd_buf, target_addr.clone())?;
+    let mut now = send_buf_to_udp_sock(&socket,
+                                       target_addr.clone(),
+                                       &mut msg_id_counter,
+                                       &mut ts_queue,
+    )?;
     loop {
         poll.poll(&mut events, Some(Duration::from_millis(read_response_timeout)))?;
 
         // poll timeout, no events
         if events.is_empty() {
             println!("Receive timeout");
-            now = send_buf_to_udp_sock(&socket, snd_buf, target_addr.clone())?;
+            ts_queue.pop_front();
+            now = send_buf_to_udp_sock(&socket,
+                                       target_addr.clone(),
+                                       &mut msg_id_counter,
+                                       &mut ts_queue,
+            )?;
             continue;
         }
 
@@ -175,25 +206,67 @@ fn run_udp_client(address: &str, port: &str, interval: Option<u64>) -> Result<()
             match event.token() {
                 UDP_SOCKET => {
                     if event.is_readable() {
-                        let mut rcv_buf = [0; 1024];
-                        match socket.recv(&mut rcv_buf) {
-                            Ok(_) => {
-                                println!("RTT = {} us", now.elapsed().as_micros());
-                            },
-                            Err(e) => {
-                                println!("Error reading: {}", e);
+                        loop {
+                            let mut rcv_buf = [0; 8];
+                            match socket.recv(&mut rcv_buf) {
+                                Ok(_) => {
+                                    let recv_msg_id = u64::from_be_bytes(rcv_buf);
+                                    loop {
+                                        match ts_queue.pop_front() {
+                                            Some(time_stamp) => {
+                                                if time_stamp.id == recv_msg_id {
+                                                    println!("RTT = {} us", time_stamp.timestamp.elapsed().as_micros());
+                                                    break;
+                                                } else {
+                                                    if time_stamp.id > recv_msg_id {
+                                                        break;
+                                                    }
+                                                }
+                                            }
+                                            None => {
+                                                break;
+                                            }
+                                        }
+                                        if now.elapsed().as_millis() >= interval.unwrap() as u128 {
+                                            break;
+                                        }
+                                    }
+                                },
+                                Err(_) => {
+                                    break;
+                                }
                             }
                         }
                         if interval.is_none() {
-                            now = send_buf_to_udp_sock(&socket, snd_buf, target_addr.clone())?;
+                            match send_buf_to_udp_sock(&socket,
+                                                       target_addr.clone(),
+                                                       &mut msg_id_counter,
+                                                       &mut ts_queue) {
+                                Ok(value) => {
+                                    now = value;
+                                }
+                                Err(_) => {
+                                    continue;
+                                }
+                            }
                         }
                     }
                 }
                 TIMEOUT => {
-                    now = send_buf_to_udp_sock(&socket, snd_buf, target_addr.clone())?;
+                    match send_buf_to_udp_sock(&socket,
+                                               target_addr.clone(),
+                                               &mut msg_id_counter,
+                                               &mut ts_queue) {
+                        Ok(value) => {
+                            now = value;
+                        }
+                        Err(_) => {
+                            continue;
+                        }
+                    }
                 }
                 Token(_) => {
-                    println!("Something went wrong");
+                    unreachable!();
                 }
             }
         }

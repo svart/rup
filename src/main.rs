baddd55133424e18e7ea939d71519e5ee7cd6da7
Dmitry Shinkaruk <dimashink@gmail.com>
2022-03-16 23:10:03 +0300

    Basic implementation of new architecture with channels

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,89 @@
-mod tcp;
-mod udp;
-mod client;
-mod server;
-mod pinger;
-mod common;
-mod icmp;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
+use std::net::UdpSocket;
 
-#[macro_use]
-extern crate clap;
+struct PingReqResp {
+    index: u32,
+}
 
-use clap::App;
-use std::io::Result;
 
-// TODO: Set tos on the messages
-// TODO: Set length of the messages
+fn tx_transport_thread(rx: Receiver<PingReqResp>) {
+    let socket = UdpSocket::bind("127.0.0.1:55555").expect("binding failed");
+    socket.connect("127.0.0.1:44444").expect("connect function failed");
 
-fn main() -> Result<()> {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
-
-    let protocol = matches.value_of("protocol").unwrap_or("icmp");
-    let server_mode = matches.is_present("server");
-    let client_mode = matches.is_present("client");
-    let interval: Option<u64> = matches.value_of("interval").map(|value| value.parse().unwrap());
-
-    if protocol == "icmp" {
-        let remote_address = matches.value_of("remote-address").unwrap();
-        icmp::run_client(remote_address, interval)
-    } else if server_mode {
-        let local_address = matches.value_of("local-address").unwrap_or("0.0.0.0");
-        let local_port = matches.value_of("local-port").unwrap();
-
-        match protocol {
-            "tcp" => tcp::run_server(local_address, local_port),
-            "udp" => udp::run_server(local_address, local_port),
-            _ => unimplemented!(),
+    loop {
+        match rx.recv() {
+            Ok(PingReqResp { index }) => {
+                println!("tx: Received {index} from client");
+                socket.send(&index.to_be_bytes()).expect("couldn't send message");
+            }
+            Err(err) => {
+                println!("tx: Got error during recv from client: {err}");
+                break;
+            }
         }
-    } else if client_mode {
-        let remote_address = matches.value_of("remote-address").unwrap();
-        let remote_port = matches.value_of("remote-port").unwrap();
-
-        match protocol {
-            "tcp" => tcp::run_client(remote_address, remote_port, interval),
-            "udp" => udp::run_client(remote_address, remote_port, interval),
-            _ => unimplemented!(),
-        }
-    } else {
-        panic!("Mode should be set or protocol should be icmp!");
     }
+}
+
+fn as_u32_be(array: &[u8; 4]) -> u32 {
+    ((array[0] as u32) << 24) +
+    ((array[1] as u32) << 16) +
+    ((array[2] as u32) <<  8) +
+    ((array[3] as u32) <<  0)
+}
+
+fn rx_transport_thread(tx: Sender<PingReqResp>) {
+    let socket = UdpSocket::bind("127.0.0.1:44444").unwrap();
+
+    loop {
+        let mut buf = [0; 4];
+        let amt = socket.recv(&mut buf).unwrap();
+        if amt == 4 {
+            let num = as_u32_be(&buf);
+            println!("rx: Received {num} from socket, sending to client");
+            tx.send(PingReqResp{ index: num }).unwrap();
+        }
+        else {
+            println!("rx: Received not full number. {amt} instead of 4.");
+            break;
+        }
+    }
+}
+
+fn client_thread(to_transport: Sender<PingReqResp>, from_transport: Receiver<PingReqResp>) {
+    for i in 0..20 {
+        thread::sleep(Duration::from_millis(500));
+
+        match to_transport.send(PingReqResp{index: i}) {
+            Ok(_) => println!("client: Sent {i} to transport"),
+            Err(err) => {
+                println!("client: Error during sending {i} to transport: {err}");
+                break;
+            }
+        }
+
+        match from_transport.recv() {
+            Ok(PingReqResp { index }) => println!("client: Received {index} from transport"),
+            Err(err) => {
+                println!("client: Error during recv from transport: {err}");
+                break;
+            }
+        }
+        println!();
+    }
+}
+
+fn main() {
+    let (client_transport_tx, client_transport_rx): (Sender<PingReqResp>, Receiver<PingReqResp>) = mpsc::channel();
+    let (transport_client_tx, transport_client_rx): (Sender<PingReqResp>, Receiver<PingReqResp>) = mpsc::channel();
+
+    let client = thread::spawn(move || client_thread(client_transport_tx, transport_client_rx));
+    let tx_transport = thread::spawn(move || tx_transport_thread(client_transport_rx));
+    let rx_transport = thread::spawn(move || rx_transport_thread(transport_client_tx));
+
+    client.join().expect("oops, client crashed");
+    tx_transport.join().expect("oops, tx transport crashed");
+    rx_transport.join().expect("oops, rx transport crashed");
 }

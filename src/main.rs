f8837715fa95446d341ed59cec56c33e2da86127
Dmitry Shinkaruk <dimashink@gmail.com>
2022-03-17 22:57:43 +0300

    New version of threaded communication

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,50 @@
+// mod tcp;
+// mod udp;
+// mod client;
+// mod server;
+// mod pinger;
+// mod common;
+// mod icmp;
+
+// #[macro_use]
+// extern crate clap;
+
+// use clap::App;
+// use std::io::Result;
+
 use std::sync::mpsc::{Sender, Receiver};
 use std::sync::mpsc;
 use std::thread;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use std::net::UdpSocket;
+use std::collections::VecDeque;
+use std::cmp::Ordering;
+
+enum ReqResp {
+    REQUEST,
+    RESPONSE,
+}
 
 struct PingReqResp {
-    index: u32,
+    index: u64,
+    timestamp: Instant,
+    t: ReqResp,
 }
 
 
-fn tx_transport_thread(rx: Receiver<PingReqResp>) {
-    let socket = UdpSocket::bind("127.0.0.1:55555").expect("binding failed");
-    socket.connect("127.0.0.1:44444").expect("connect function failed");
+fn tx_transport_thread(from_client: Receiver<PingReqResp>,
+                       to_client: Sender<PingReqResp>) {
+    let socket = UdpSocket::bind("127.0.0.1:55555").expect("tx: binding failed");
+    socket.connect("127.0.0.1:44444").expect("tx: connect function failed");
 
     loop {
-        match rx.recv() {
-            Ok(PingReqResp { index }) => {
+        match from_client.recv() {
+            Ok(mut req) => {
+                let index = req.index;
+                req.timestamp = Instant::now();
                 println!("tx: Received {index} from client");
-                socket.send(&index.to_be_bytes()).expect("couldn't send message");
+                socket.send(&index.to_be_bytes()).expect("tx: couldn't send message");
+                to_client.send(req).expect("tx: couldn't send transformed request to client");
             }
             Err(err) => {
                 println!("tx: Got error during recv from client: {err}");
@@ -27,45 +54,60 @@ fn tx_transport_thread(rx: Receiver<PingReqResp>) {
     }
 }
 
-fn as_u32_be(array: &[u8; 4]) -> u32 {
-    ((array[0] as u32) << 24) +
-    ((array[1] as u32) << 16) +
-    ((array[2] as u32) <<  8) +
-    ((array[3] as u32) <<  0)
-}
-
-fn rx_transport_thread(tx: Sender<PingReqResp>) {
+fn rx_transport_thread(to_client: Sender<PingReqResp>) {
     let socket = UdpSocket::bind("127.0.0.1:44444").unwrap();
 
     loop {
-        let mut buf = [0; 4];
+        let mut buf = [0; 8];
         let amt = socket.recv(&mut buf).unwrap();
-        if amt == 4 {
-            let num = as_u32_be(&buf);
+        if amt == 8 {
+            let num = u64::from_be_bytes(buf);
             println!("rx: Received {num} from socket, sending to client");
-            tx.send(PingReqResp{ index: num }).unwrap();
+            to_client.send(PingReqResp{ index: num, timestamp: Instant::now(), t: ReqResp::RESPONSE }).unwrap();
         }
         else {
-            println!("rx: Received not full number. {amt} instead of 4.");
+            println!("rx: Received not full number. {amt} instead of 8.");
             break;
         }
     }
 }
 
-fn client_thread(to_transport: Sender<PingReqResp>, from_transport: Receiver<PingReqResp>) {
+fn client_generator(to_tx_transport: Sender<PingReqResp>) {
     for i in 0..20 {
-        thread::sleep(Duration::from_millis(500));
-
-        match to_transport.send(PingReqResp{index: i}) {
+        match to_tx_transport.send(PingReqResp{index: i, timestamp: Instant::now(), t: ReqResp::REQUEST}) {
             Ok(_) => println!("client: Sent {i} to transport"),
-            Err(err) => {
-                println!("client: Error during sending {i} to transport: {err}");
-                break;
-            }
+            Err(err) => panic!("client: Error during sending {i} to transport: {err}"),
         }
 
+        thread::sleep(Duration::from_millis(500));
+    }
+}
+
+fn client_statistic(from_transport: Receiver<PingReqResp>) {
+    let mut requests: VecDeque<PingReqResp> = VecDeque::new();
+
+    loop {
         match from_transport.recv() {
-            Ok(PingReqResp { index }) => println!("client: Received {index} from transport"),
+            Ok(resp) => {
+                match resp.t {
+                    ReqResp::REQUEST => {
+                        requests.push_back(resp);
+                    },
+                    ReqResp::RESPONSE => {
+                        let index = resp.index;
+                        println!("client: Received {index} from transport");
+
+                        while let Some(req) = requests.pop_front() {
+                            match index.cmp(&req.index) {
+                                Ordering::Greater => continue,
+                                Ordering::Equal => println!("Seq: {index} rtt: {:#?}", resp.timestamp.duration_since(req.timestamp)),
+                                Ordering::Less => requests.push_front(req),
+                            }
+                            break;
+                        }
+                    },
+                }
+            }
             Err(err) => {
                 println!("client: Error during recv from transport: {err}");
                 break;
@@ -76,14 +118,18 @@ fn client_thread(to_transport: Sender<PingReqResp>, from_transport: Receiver<Pin
 }
 
 fn main() {
-    let (client_transport_tx, client_transport_rx): (Sender<PingReqResp>, Receiver<PingReqResp>) = mpsc::channel();
-    let (transport_client_tx, transport_client_rx): (Sender<PingReqResp>, Receiver<PingReqResp>) = mpsc::channel();
+    let (cl_txtr_tx, cl_txtr_rx): (Sender<PingReqResp>, Receiver<PingReqResp>) = mpsc::channel();
+    let (txtr_cl_tx, tr_cl_rx): (Sender<PingReqResp>, Receiver<PingReqResp>) = mpsc::channel();
+    let rxtr_cl_tx = txtr_cl_tx.clone();
 
-    let client = thread::spawn(move || client_thread(client_transport_tx, transport_client_rx));
-    let tx_transport = thread::spawn(move || tx_transport_thread(client_transport_rx));
-    let rx_transport = thread::spawn(move || rx_transport_thread(transport_client_tx));
 
-    client.join().expect("oops, client crashed");
+    let generator = thread::spawn(move || client_generator(cl_txtr_tx));
+    let tx_transport = thread::spawn(move || tx_transport_thread(cl_txtr_rx, txtr_cl_tx));
+    let rx_transport = thread::spawn(move || rx_transport_thread(rxtr_cl_tx));
+    let statista = thread::spawn(move || client_statistic(tr_cl_rx));
+
+    generator.join().expect("oops, generator crashed");
     tx_transport.join().expect("oops, tx transport crashed");
     rx_transport.join().expect("oops, rx transport crashed");
+    statista.join().expect("oops, statista crashed")
 }
